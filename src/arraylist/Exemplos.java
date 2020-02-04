ea605fb82a646c596daec6544e302815945f2df4
glaydson <glaydsonvs@gmail.com>
2020-02-04 13:06:24 -0200

    Atualização do código de ArrayList e Generics Unidade 1

--- src/arraylist/Exemplos.java
+++ src/arraylist/Exemplos.java
@@ -1,54 +1,69 @@
 package arraylist;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class Exemplos {
 
 	public static void main(String[] args) {
-		List<Long> lista = new ArrayList<>();
-		 
-		lista.add(1L);
-		lista.add(2L);
-		lista.add(1, 3L);
-		
-		List<Integer> list = new ArrayList<>(
-				  IntStream.range(0, 10).boxed().collect(toCollection(ArrayList::new))
-				);
-				ListIterator<Integer> it = list.listIterator(list.size());
-				List<Integer> result = new ArrayList<>(list.size());
-				while (it.hasPrevious()) {
-				    result.add(it.previous());
-				}
-				 
-				Collections.reverse(list);	
-				
-				List<String> list = LongStream.range(0, 16)
-						  .boxed()
-						  .map(Long::toHexString)
-						  .collect(toCollection(ArrayList::new));
-						List<String> stringsToSearch = new ArrayList<>(list);
-						stringsToSearch.addAll(list);
 
-						List<String> copy = new ArrayList<>(stringsToSearch);
-						Collections.sort(copy);
-						int index = Collections.binarySearch(copy, "f");
-						
-						
-						List<Integer> list = new ArrayList<>(
-								  IntStream.range(0, 10).boxed().collect(toCollection(ArrayList::new))
-								);
-								Collections.reverse(list);
-								 
-								list.remove(0);
-								assertThat(list.get(0), equalTo(8));
-								 
-								list.remove(Integer.valueOf(0));
-						
+		// CONSTRUTOR DE ARRAYLIST ACEITANDO UMA COLEÇÃO
+		Collection<Integer> numeros = IntStream.range(0, 10).boxed().collect(Collectors.toSet());
+		List<Integer> lista1 = new ArrayList<>(numeros);
+		lista1.forEach(System.out::println);
+
+		// ADICIONAR ELEMENTOS A UM ARRAYLIST
+		List<Long> lista2 = new ArrayList<>();
+		lista2.add(1L);
+		lista2.add(2L);
+		lista2.add(1, 3L);
+		lista2.forEach(System.out::println);
+
+		// PERCORRENDO O ARRAYLIST COM LISTITERATOR
+		List<Integer> lista3 = new ArrayList<>(
+				IntStream.range(0, 10).boxed().collect(Collectors.toCollection(ArrayList::new)));
+		ListIterator<Integer> it = lista3.listIterator(lista3.size());
+		// Criando uma lista com os valores invertidos
+		List<Integer> resultado = new ArrayList<>(lista3.size());
+		while (it.hasPrevious()) {
+			resultado.add(it.previous());
+		}
+		resultado.forEach(System.out::println);
+
+		// BUSCANDO ELEMENTOS
+		List<String> lista4 = LongStream.range(0, 16).boxed().map(Long::toHexString)
+				.collect(Collectors.toCollection(ArrayList::new));
+		List<String> stringsParaBuscar = new ArrayList<>(lista4);
+		stringsParaBuscar.addAll(lista4);
+		stringsParaBuscar.forEach(System.out::println);
+
+		// Buscando na coleção não ordenada
+		System.out.println(stringsParaBuscar.indexOf("a"));
+		System.out.println(stringsParaBuscar.lastIndexOf("a"));
+		
+		// Buscando em uma coleção ordenada
+		List<String> copia = new ArrayList<>(stringsParaBuscar);
+		Collections.sort(copia);
+		int index = Collections.binarySearch(copia, "f");
+		System.out.println(index);
+
+		// REMOVENDO UM ELEMENTO
+		List<Integer> lista5 = new ArrayList<>(
+				IntStream.range(0, 10).boxed().collect(Collectors.toCollection(ArrayList::new)));
+		Collections.reverse(lista5);
+
+		lista5.remove(0);   // Deve remover o número 9, que está no índice 0
+		lista5.forEach(System.out::println);
+
+		lista5.remove(Integer.valueOf(0));  // Deve remover o número 0
+		lista5.forEach(System.out::println);
+
 	}
 
 }
--- src/generics/Exemplos.java
+++ src/generics/Exemplos.java
@@ -6,19 +6,23 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Exemplo1 {
+public class Exemplos {
 
 	public static void main(String[] args) {
+		
+		// CRIANDO UMA LISTA DE INTEIROS
 		List<Integer> lista = new LinkedList<>();
 		lista.add(new Integer(1));
 		Integer i = lista.iterator().next();
 
 	}
 
+	// MÉTODO GENÉRICO PARA CONVERTER UM ARRAY PARA UMA LISTA
 	public <T> List<T> deArrayParaLista(T[] a) {
 		return Arrays.stream(a).collect(Collectors.toList());
 	}
 	
+	// MÉTODO GENÉRICO PARA CONVERTER UM ARRAY DE UM TIPO T PARA UMA LISTA DE UM TIPO G
 	public static <T, G> List<G> deArrayParaLista(T[] a, Function<T, G> funcaoMapeadora) {
 	    return Arrays.stream(a)
 	      .map(funcaoMapeadora)
